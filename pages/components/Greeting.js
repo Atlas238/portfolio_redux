19be3e8cdc01c501591b91e0f17aaba85b9597dd
Atlas238 <jack@bentsen.us>
2022-04-20 11:11:18 -0700

    updating greeting

--- pages/components/Greeting.js
+++ pages/components/Greeting.js
@@ -6,7 +6,10 @@ export default function Greeting({ theme }) {
       <h3>Hi! My name is Jack and I am a Full Stack Web Developer.</h3>
       <p>
         A recent graduate of The University of Washington Full-Stack Coding
-        Bootcamp program, looking to learn with a focus in React.
+        Bootcamp program, looking to learn with a focus in React. I am currently enrolled
+        in the Seattle U Computer Science Fundamentals Certificate course with the intention
+        of continuing on to complete my masters. Feel free to reach out if there's something
+        you'd like to work on together!
       </p>
     </div>
   );
